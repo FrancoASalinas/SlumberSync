394a13a1e6cdab9327085628aca4c2790b9887c2
FrancoSMiño <frankow222@gmail.com>
2024-06-06 16:48:29 -0300

    feat: pause sparkles animations when not in the viewport.

--- Sparkles.js
+++ Sparkles.js
@@ -1,48 +1,91 @@
-const heroSection = document.getElementsByClassName(
-  'hero-section__image-container'
-)[0];
-
-export default class Sparkles {
-  constructor(quantity) {
-    this.quantity = quantity;
+class Sparkle {
+  constructor(parentElement) {
+    this._element = this._createElement(parentElement);
+    this._tween = undefined;
+    this._setInitialPosition();
+    this._from();
+    this._animate();
   }
 
-  _setInitialPosition(elementClass) {
-    gsap.set(elementClass, {
+  _createElement(parentElement) {
+    const sparkle = document.createElement('div');
+    sparkle.className = 'sparkle';
+    parentElement.appendChild(sparkle);
+
+    return sparkle;
+  }
+
+  _setInitialPosition() {
+    gsap.set(this._element, {
       top: 'random(0, 100)%',
       left: 'random(0, 100)%',
     });
   }
 
-  createSparkles() {
-    for (let i = 0; i < this.quantity; i++) {
-      const sparkle = document.createElement('div');
-      sparkle.className = `sparkle${i} sparkle`;
-      heroSection.appendChild(sparkle);
-
-      this._setInitialPosition(`.sparkle${i}`);
-    }
+  _from() {
+    gsap.from(this._element, {
+      opacity: 0,
+      delay: 'random(0,5)',
+      duration: 'random(0,2)',
+    });
   }
 
-  _startAnimationLoop(elementClass) {
-    gsap.from(elementClass, { opacity: 0, duration: 2 });
-
-    const animation = () =>
-      gsap
-        .to(elementClass, {
-          top: 'random(0, 100)%',
-          left: 'random(0, 100)%',
-          duration: 'random(5, 8)',
-          ease: 'power2.out',
-        })
-        .then(() => animation());
-
-    animation();
+  _animate() {
+    this._tween = gsap.to(this._element, {
+      top: 'random(0, 100)%',
+      left: 'random(0, 100)%',
+      duration: 'random(5, 8)',
+      ease: 'power2.out',
+      onComplete: () => this._animate(),
+    });
   }
 
-  animateSparkles() {
-    for (let i = 0; i < this.quantity; i++) {
-      this._startAnimationLoop(`.sparkle${i}`);
-    }
+  pause() {
+    this._tween.pause();
+  }
+
+  get isPaused() {
+    return this._tween && this._tween.paused;
+  }
+
+  resume() {
+    this._tween.resume();
+  }
+}
+
+export default class Sparkles {
+  constructor(quantity, parentElement) {
+    this._sparklesList = this._createSparkles(quantity, parentElement);
+    this._parentElement = parentElement;
+
+    this._pauseWhenNotInView();
+  }
+
+  _createSparkles(quantity, parentElement) {
+    return new Array(quantity)
+      .fill()
+      .map(slot => (slot = new Sparkle(parentElement)));
+  }
+
+  _pauseWhenNotInView() {
+    window.addEventListener('scroll', () => {
+      if (!this._isInViewport(this._parentElement)) {
+        this._pause();
+      } else {
+        this._resume();
+      }
+    });
+  }
+
+  _isInViewport(element) {
+    return element.getBoundingClientRect().bottom >= 0;
+  }
+
+  _pause() {
+    this._sparklesList.map(sparkle => sparkle.pause());
+  }
+
+  _resume() {
+    this._sparklesList.map(sparkle => sparkle.isPaused && sparkle.resume());
   }
 }
--- main.js
+++ main.js
@@ -1,39 +1,43 @@
 import Sparkles from './Sparkles.js';
 
+const heroSection = document.getElementsByClassName(
+  'hero-section__image-container'
+)[0];
+
 gsap.registerPlugin(ScrollTrigger);
 
-const SPARKLES_QUANTITY = 10;
-const sparkles = new Sparkles(SPARKLES_QUANTITY);
+new Sparkles(10, heroSection);
 
-sparkles.createSparkles();
-sparkles.animateSparkles();
+scrollAnimation();
 
-const tl = gsap.timeline({
-  scrollTrigger: {
-    trigger: '.scroll-container',
-    start: 'bottom bottom',
-    scrub: 1,
-    pin: true,
-    anticipatePin: 1,
-    snap: {
-      snapTo: 'labelsDirectional',
-      duration: { min: 0.2, max: 2 },
-      delay: 0,
+function scrollAnimation() {
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.scroll-container',
+      start: 'bottom bottom',
+      scrub: 1,
+      pin: true,
+      anticipatePin: 1,
+      snap: {
+        snapTo: 'labelsDirectional',
+        duration: { min: 0.2, max: 2 },
+        delay: 0,
+      },
     },
-  },
-});
+  });
 
-tl.add('start')
-  .to(
-    'body',
-    {
-      background: 'linear-gradient(#0a1529, #141924)',
-    },
-    'start'
-  )
-  .to('.scroll-container', { x: '-50%' }, 'start')
-  .add('end');
+  tl.add('start')
+    .to(
+      'body',
+      {
+        background: 'linear-gradient(#0a1529, #141924)',
+      },
+      'start'
+    )
+    .to('.scroll-container', { x: '-50%' }, 'start')
+    .add('end');
 
-gsap.set('body', {
-  background: 'linear-gradient(#0a1529, #1b3464)',
-});
+  gsap.set('body', {
+    background: 'linear-gradient(#0a1529, #1b3464)',
+  });
+}
